506ffbe12b61f332d8084b18fff1ff616ab296bb
Ndn1618 <ndilbar1618@gmail.com>
2020-11-04 13:56:46 +0500

    Add chat with Lobar (on process)

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,12 @@ var checkboxLink = document.querySelector('.checkbox-link');
 var checkboxInput = document.querySelector('.checkbox-input');
 
 checkboxLink.addEventListener('click', function () {
-  if(checkboxInput.checked == false) {
+  if (checkboxInput.checked == false) {
     checkboxInput.checked = true;
   }
   else {
-    if(checkboxInput.checked == true) {
-        checkboxInput.checked = false;
-     }
+    if (checkboxInput.checked == true) {
+      checkboxInput.checked = false;
+    }
   }
 });
